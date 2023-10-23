6c45e54cae35272a3d16c1500ecf3d05f652ebfc
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-10-23 19:13:41 +0700

    change response type

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.status(200);
-  res.send("Halo Bang....!");
+  res.status(200).json({
+    status: "success",
+    message: "Hello World!",
+  });
 });
 
 app.listen(PORT, () => {
